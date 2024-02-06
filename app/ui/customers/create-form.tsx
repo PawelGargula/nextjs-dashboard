1b3967bda78929dea317a2df93b73e81636a3c58
Paweł Gargula <pawel.gargula@interia.pl>
2024-02-06 17:47:54 +0100

    Set proper input type for Email field

--- app/ui/customers/create-form.tsx
+++ app/ui/customers/create-form.tsx
@@ -55,7 +55,7 @@ export default function Form() {
               <input
                 id="email"
                 name="email"
-                type="text"
+                type="email"
                 placeholder="Enter Email"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby='email-error'
